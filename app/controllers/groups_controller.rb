21ce6bcfc7a634377e00614ba0fe4ceafc0751ae
maxgta <maxgta@163.com>
2017-04-20 23:09:53 -0700

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,23 +1,28 @@
 class GroupsController < ApplicationController
   def index
     @groups = Group.all
-  end
+end
   def show
    @group = Group.find(params[:id])
- end
+end
  def edit
   @group = Group.find(params[:id])
- end
+end
   def new
    @group = Group.new
- end
+end
  def create
    @group = Group.new(group_params)
    @group.save
 
      redirect_to groups_path
- end
-
+end
+def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to groups_path
+end
  private
 
  def group_params
