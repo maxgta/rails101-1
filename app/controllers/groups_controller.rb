12ef0503ab5af38902fc44e4555a496b36aa854e
maxgta <maxgta@163.com>
2017-04-21 05:55:23 -0700

    add validation to title apply to edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,10 +13,12 @@ end
 end
  def create
    @group = Group.new(group_params)
-   @group.save
-
-     redirect_to groups_path
-end
+   if @group.save
+         redirect_to groups_path
+       else
+         render :new
+       end
+     end
 def destroy
     @group = Group.find(params[:id])
     @group.destroy
